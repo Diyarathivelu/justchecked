ba9d894bdeeb3c0c6d7790a457169afff35632a8
Dhivyabharathi <116085250+Diyarathivelu@users.noreply.github.com>
2025-07-30 17:08:48 +0530

    Update index.php

--- justchecked/index.php
+++ justchecked/index.php
@@ -10,11 +10,13 @@ include_once 'encrypt.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JustChecked</title>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="bootstrap/js/bootstrap.bundle.min.js">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <script src="bootstrap/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="./fontawsome/css/all.min.css">   
-    <script src="jquery/jquery-3.7.1.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <style>
       
        .header, footer,.btn-primary{
